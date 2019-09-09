83c4b188bcfd9f63a678d70357793becf74e9241
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2019-09-09 21:49:33 +0200

    Update cell_ranger.sh

--- cellranger count/cell_ranger.sh
+++ cellranger count/cell_ranger.sh
@@ -19,3 +19,29 @@ cd $PBS_O_WORKDIR
 #echo "Current job name : $JOB_NAME"
 #echo "Task index number : $SGE_TASK_ID"
 #echo "=========================================================="
+
+
+genome=~/genome/10x_genome/mouse_genome
+data=~/single_cell/fastqs
+name=$1
+
+cellranger count --chemistry=SC3Pv3 \
+--id=${name}-REX --project=P180721 \
+--transcriptome=${genome} \
+--fastqs=${data} \
+--sample=${name} \
+--localcores=7 \
+--localmem=128
+
+
+
+#cellranger count --chemistry=SC3Pv3 \
+#--id=633818_28-REX --project=P180721 \
+#--transcriptome=~/genome/10x_genome/mouse_genome \
+#--fastqs=~/single_cell/fastqs \
+#--sample=633818_28 \
+#--localcores=7 \
+#--localmem=128
+
+cellranger count --id=control_2 --sample=63381822 --fastqs=~/single_cell/fastqs/ --transcriptome=~/genome/10x_genome/mouse_genome --expect-cells=500
+
